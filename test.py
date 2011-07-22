fa32d6357ba7ace1fe38c9014df72c7211d40e49
Kailash Budhathoki <kailash.buki@gmail.com>
2011-07-22 15:53:52 +0545

    testing testing testing

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestExample(unittest.TestCase):
         sample description   
     """
     def test_always_true(self):
-        self.assertEqual(2, 1)
+        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
